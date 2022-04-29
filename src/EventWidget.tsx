f7cbd2c97fb49603d14ad2fb136aadb61a35ed0f
Alexandre TRICHOT <contact@alexandretrichot.fr>
2022-04-29 21:22:50 +0200

    initial commit

---
+++ src/EventWidget.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export type EventWidgetProps = {
+
+}
+
+export const EventWidget: React.FC<EventWidgetProps> = ({
+
+}) => {
+	return <div>
+		
+	</div>;
+}
---
+++ src/WeekPicker.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export type WeekPickerProps = {};
+
+export const WeekPicker: React.FC<WeekPickerProps> = () => {
+  return <div className='bg-red'>Hey</div>;
+};
